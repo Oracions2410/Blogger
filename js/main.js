7cc2d6778f01424c45ac191e023cbad5654fa9db
Oracions <Oracions.dev@gmail.com>
2020-07-10 17:06:31 +0100

    Owl Carousel

--- js/main.js
+++ js/main.js
@@ -8,4 +8,17 @@ $(document).ready(function () {
     $toggleCollapse.click(function () {
         $nav.toggleClass('collapse')
     })
+
+
+    /* Owl Carousel for blog */
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autotimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    })
+
+
 })
